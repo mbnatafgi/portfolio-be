9dac60640644c65080380736a0937df99204425b
mbnatafgi <bilal.natafji@gmail.com>
2020-04-30 01:32:54 +0300

    revert port to 8000

--- src/server.py
+++ src/server.py
@@ -41,7 +41,7 @@ async def connect(websocket, path):
 
 
 def main():
-    start_server = serve(connect, '0.0.0.0', 8001)
+    start_server = serve(connect, '0.0.0.0', 8000)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
 
